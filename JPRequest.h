d969879293528b66d6688ba554c8098fed967cb6
iMac0de <jeremy.peltier@epitech.eu>
2014-06-02 23:39:49 +0200

    Update header

--- JPRequest.h
+++ JPRequest.h
@@ -1,9 +1,8 @@
 //
 //  JPRequest.h
-//  Jobapic
 //
 //  Created by Jeremy Peltier on 02/06/2014.
-//  Copyright (c) 2014 idtonic. All rights reserved.
+//  Copyright (c) 2014 Jeremy Peltier. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
